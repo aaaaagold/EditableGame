e3db4db42c83670ae5e42a04bcec05796084cd76
aaaaagold <aaaaagold@aaaaagold.aaaaagold>
2021-11-15 13:26:28 +0800

    init base

---
+++ docs/js/plugins/Agold_AutoStretch.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+{ let p=Scene_Boot.prototype,r,k;
+k='start';
+r=p[k]; (p[k]=function f(){
+	{
+		let g=Graphics,gw=g.widrg,gh=g.height,t=window,w=t.innerWidth,h=t.innerHeight;
+		g.hideFps();
+		g._switchFPSMeter();
+		g._switchFPSMeter();
+		if(!g._stretchEnabled && ( gw>w || gh>h || (gw+(gw>>1)<w&&gh+(gh>>1)<h) ) ) Graphics._switchStretchMode();
+	}
+	return f.ori.apply(this,arguments);
+}).ori=r;
+}
---
+++ docs/js/plugins/Agold_ChangeFont.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+
+{ let p=Graphics,r,k;
+k='_createGameFontLoader';
+r=p[k]; (p[k]=function(){}).ori=r;
+}
+
+{ let a=Bitmap,p=a.prototype,r,k;
+a.fontFace="Consolas,'Courier New',Courier,微軟正黑體,標楷體,monospace";
+k='initialize';
+r=p[k]; (p[k]=function f(){
+	const rtv=f.ori.apply(this,arguments);
+	this.fontFace=Bitmap.fontFace;
+	return rtv;
+}).ori=r;
+}
+
+{ let p=Scene_Boot.prototype,r,k;
+k='isGameFontLoaded';
+r=p[k]; (p[k]=function f(){
+	if(Graphics.isFontLoaded( Bitmap.fontFace ) || Graphics.canUseCssFontLoading()) return true;
+	else if(Date.now()-this._startDate>=20000){
+		setTimeout(()=>alert("ERROR: Font not loaded."),1);
+		throw new Error('Failed to load font');
+	}
+}).ori=r;
+}
+
+{ let p=Window_Base.prototype,r,k;
+k='standardFontFace';
+r=p[k]; (p[k]=function f(){
+	if($gameSystem.isKorean()) return Bitmap.fontFace+',Dotum, AppleGothic, sans-serif';
+	else return Bitmap.fontFace;
+}).ori=r;
+}
